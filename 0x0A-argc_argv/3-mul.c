0a4707ed631c0cb07816ba4280825b5f809ce31f
bamfi <125449009+bamfi@users.noreply.github.com>
2023-04-08 16:56:53 +0000

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -40,20 +40,22 @@ int digit = 0;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
-		} i++;	
+		}
+		i++;	
 	}
 	if (f == 0)
 	return (0);
-    return (n);		
+return (n);		
 }
 
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
- * @argv: array of arguments		
+ * @argv: array of arguments
+ *
  * Return: 0 (Success), 1 (Error)
  */
-int main(int argc, char *argv[])		
+int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 	if (argc < 3 || argc > 3)	
