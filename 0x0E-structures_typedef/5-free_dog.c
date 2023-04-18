627434187b9f41a475082949cdc073156bfdb9ef
Firdaous Bamoulid <bamoulid.firdaous@gmail.com>
2023-04-17 17:57:48 -0700

    task5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,20 +1,19 @@
 #include <stdlib.h>
 #include "dog.h"
 
-
 /**
 * free_dog - frees dogs
-* @d: pointer 
+* @d: Pointer to free dog s info
 *
 * Return: void
 */
 void free_dog(dog_t *d)
 {
-   if (d)
-   {
-       free(d->name);
-       free(d->owner);
-       free(d);
-   }
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
 
