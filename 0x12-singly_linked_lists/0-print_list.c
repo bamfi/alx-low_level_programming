08047e0b4dbf76bdd04aa57636906fe234561697
Firdaous Bamoulid <bamoulid.firdaous@gmail.com>
2023-04-28 15:19:32 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,16 @@
  */
 size_t print_list(const list_t *h)
 {
- size_t trucknum = 0;
+size_t trucknum = 0;
 
- while (h)
- {
- if (!h->str)
- printf("[0] (nil)\n");
- else
- printf("[%u] %s\n", h->len, h->str);
- h = h->next;
- trucknum++;
- }
-
- return (trucknum);
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+trucknum++;
+}
+return (trucknum);
 }
