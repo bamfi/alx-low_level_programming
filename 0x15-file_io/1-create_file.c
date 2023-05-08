4e60cb2f8a95b168df8ea75a6139502c301ea4d3
Firdaous Bamoulid <bamoulid.firdaous@gmail.com>
2023-05-08 14:59:22 -0700

    createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,9 +4,8 @@
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success,-1 on failure
  *
- * Return: Returns: 1 on success
- *                 -1 on failure         
  */
 int create_file(const char *filename, char *text_content)
 {
